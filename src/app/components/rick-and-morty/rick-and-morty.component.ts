d6fc870a3a373506919ab559c105cbb0efbe23b8
AlekseiiKanaiev <alexey.kanaev.ua@gmail.com>
2019-12-14 10:16:10 +0200

    create table with characters

--- src/app/components/rick-and-morty/rick-and-morty.component.ts
+++ src/app/components/rick-and-morty/rick-and-morty.component.ts
@@ -1,20 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { GetDataService } from 'src/app/_services/getData.service';
 import { Data } from 'src/app/_models/data.model';
 import { Character } from 'src/app/_models/character.model';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-rick-and-morty',
   templateUrl: './rick-and-morty.component.html',
   styleUrls: ['./rick-and-morty.component.scss']
 })
-export class RickAndMortyComponent implements OnInit {
+export class RickAndMortyComponent implements OnInit, OnDestroy {
   characters: Character[];
+  private sub: Subscription;
 
   constructor(private gts: GetDataService) { }
 
   ngOnInit() {
-    this.gts.getData().subscribe(
+    this.sub = this.gts.getData().subscribe(
       (data: Data) => {
         console.log(data);
         this.characters = data.results;
@@ -23,4 +26,7 @@ export class RickAndMortyComponent implements OnInit {
     );
   }
 
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
 }
