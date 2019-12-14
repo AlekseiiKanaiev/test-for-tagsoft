c7ee5b5ceb46efc3f02fbf94750d76a319e495e4
AlekseiiKanaiev <alexey.kanaev.ua@gmail.com>
2019-12-14 09:31:22 +0200

    create structure and routings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RickAndMortyComponent } from './components/rick-and-morty/rick-and-morty.component';
+import { LayoutComponent } from './components/layout/layout.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'rick-and-morty', pathMatch: 'full'},
+  {path: 'rick-and-morty', component: RickAndMortyComponent},
+  {path: 'layout', component: LayoutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RickAndMortyComponent } from './components/rick-and-morty/rick-and-morty.component';
+import { LayoutComponent } from './components/layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RickAndMortyComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
