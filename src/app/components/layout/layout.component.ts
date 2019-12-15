83c8d12e4a1b1a0c04438aa310152fcc222eb984
AlekseiiKanaiev <alexey.kanaev.ua@gmail.com>
2019-12-15 20:10:57 +0200

    finished with responsive

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,16 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, HostListener, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss']
 })
-export class LayoutComponent implements OnInit {
+export class LayoutComponent implements OnInit, AfterViewInit {
   barValue = 75;
+
+  @ViewChild('bgimg', {static: false}) inner: ElementRef;
+  @ViewChild('numbox', {static: false}) numbox: ElementRef;
+
+  @HostListener('window:resize', ['$event.target'])
+  onResize() {
+    this.resize();
+  }
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.resize();
+  }
+
+  private resize() {
+    console.log(this.inner);
+    const height = this.inner.nativeElement.clientHeight;
+    const width = this.inner.nativeElement.clientWidth;
+    this.inner.nativeElement.style.backgroundSize = `${width}px ${height}px`;
+    this.numbox.nativeElement.style.top = `calc(50% - ${height / 2 + 4}px)`;
   }
 
   onClick() {
